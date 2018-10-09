3ddb3e098e5a580f5606571db0c4ba0b057f6350
Elena Ciumacova <elenaciumacova3@gmail.com>
2018-10-09 13:52:21 +0300

    Add files via upload

---
+++ Exercise/src/exercise/Exercise.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise;
+
+/**
+ *
+ * @author nusica33
+ */
+public class Exercise {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        long scale = 6798878799768787878L;
+        int myInt = (int) scale;
+        System.out.println("Number of the scale" + scale );
+        System.out.println("enter number" + myInt);
+        
+        String booleanString = "true";
+        boolean booleanPrimitive1 = Boolean.parseBoolean("booleanString");
+        boolean booleanPrimitive2 = Boolean.parseBoolean ("true");
+        boolean booleanPrimitive3 = Boolena.parseBoolen ("")
+        
+        int intPrimitive = Integer.parseInt("intString");
+        
+     System.out.println (booleanPrimitive1);
+     System.out.println (booleanPrimitive2);
+     System.out.println (intPrimitive1);
+    }
+    
+}
